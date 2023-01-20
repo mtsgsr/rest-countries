e2e6243a0fd3a32902bca0023713616cabdd49e9
Matheus Gomes <maatheus.gsr@gmail.com>
2023-01-20 15:14:17 -0300

    Added abort controller to request effect and observer effect with countriesVisible state to increase performance

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -5,26 +5,52 @@ import { Link } from "react-router-dom";
 
 const Cards = ({ selection, searchInput, setCountry }) => {
   const { request, data, error, loading } = useFetch();
+  const abortController = new AbortController();
+  const [countriesVisible, setCountriesVisible] = React.useState(0);
+  const ref = React.useRef(null);
 
   React.useEffect(() => {
-    let ignore = false;
     request("https://restcountries.com/v3.1/all");
     return () => {
-      ignore = true;
+      abortController.abort();
     };
   }, []);
 
   const countries = React.useMemo(() => {
     if (selection !== "") {
+      setCountriesVisible(10);
       return data.filter((data) => data.region === selection);
     } else if (searchInput.length > 1) {
+      setCountriesVisible(10);
       return data.filter((data) =>
         data.name.common.toLowerCase().includes(searchInput.toLowerCase())
       );
     } else {
+      setCountriesVisible(10);
       return data;
     }
   }, [data, selection, searchInput]);
+
+  React.useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          setCountriesVisible((countries) => countries + 4);
+        }
+      },
+      {
+        rootMargin: "0px 0px 50% 0px",
+      }
+    );
+
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+
+    return () => {
+      observer.disconnect();
+    };
+  }, [countries]);
 
   if (error) return <p className={styles.error}>{error}</p>;
 
@@ -36,40 +62,45 @@ const Cards = ({ selection, searchInput, setCountry }) => {
     );
 
   return (
-    <section className={styles.cards}>
-      {countries
-        .sort((a, b) => (a.name.common > b.name.common ? 1 : -1))
-        .map((data) => (
-          <Link
-            to={`country/${data.name.common.toLowerCase().replace(/\s/g, "-")}`}
-            className={styles.card + " fadeInLeft"}
-            key={data.cca3}
-            onClick={() => setCountry(data)}
-          >
-            <div className={styles.flag}>
-              <img
-                src={data.flags.png}
-                alt={data.name.common}
-                draggable="false"
-              />
-            </div>
-
-            <h4 key={data.cca2}>
-              <b>{data.name.common}</b>
-            </h4>
-            <span>
-              <b className="semiBold">Population:</b>{" "}
-              {data.population.toLocaleString()}
-            </span>
-            <span>
-              <b className="semiBold">Region:</b> {data.region}
-            </span>
-            <span>
-              <b className="semiBold">Capital:</b> {data.capital || "—"}
-            </span>
-          </Link>
-        ))}
-    </section>
+    <main>
+      <section className={styles.cards}>
+        {countries
+          .sort((a, b) => (a.name.common > b.name.common ? 1 : -1))
+          .slice(0, countriesVisible)
+          .map((data) => (
+            <Link
+              to={`country/${data.name.common
+                .toLowerCase()
+                .replace(/\s/g, "-")}`}
+              className={styles.card + " fadeInLeft"}
+              key={data.cca3}
+              onClick={() => setCountry(data)}
+            >
+              <div className={styles.flag}>
+                <img
+                  src={data.flags.png}
+                  alt={data.name.common}
+                  draggable="false"
+                />
+              </div>
+              <p key={data.cca2}>
+                <b>{data.name.common}</b>
+              </p>
+              <span>
+                <b className="semiBold">Population:</b>{" "}
+                {data.population.toLocaleString()}
+              </span>
+              <span>
+                <b className="semiBold">Region:</b> {data.region}
+              </span>
+              <span>
+                <b className="semiBold">Capital:</b> {data.capital || "—"}
+              </span>
+            </Link>
+          ))}
+      </section>
+      <div ref={ref} />
+    </main>
   );
 };
 
